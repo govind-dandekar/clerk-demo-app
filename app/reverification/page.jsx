35d09731b03de9afcf831fb4003bfbfaf12cdc9a
Govind Dandekar <govind.dandekar@gmail.com>
2025-06-24 12:05:21 -0700

    update reverification page with FVA time in minutes

--- app/reverification/page.jsx
+++ app/reverification/page.jsx
@@ -1,18 +1,24 @@
 "use client"
 
-import { useReverification } from '@clerk/nextjs'
+import { useReverification, useSession } from '@clerk/nextjs'
 
 import { myAction } from  '../actions';
 
 import { useRouter } from 'next/navigation'
 
+import { useEffect } from "react";
+
 export default function ReverificationPage(){
+
+  // page is protected by middleware and requires a logged-in session 
+  const { session } = useSession();
+  console.log(session?.factorVerificationAge);
+
   const performAction  = useReverification(myAction)
 
   const router = useRouter();
 
   const handleReverificationClick = async () => {
-    
     
     const myData = await performAction();
     // If `myData` is null, the user cancelled the reverification process
@@ -26,9 +32,14 @@ export default function ReverificationPage(){
   }
 
   return (
-    <button className="bg-blue-600 text-white mt-6 px-6 py-2 hover:bg-blue-800 hover:scale-105" onClick={handleReverificationClick}>
-        useReverification() test
-    </button>
+    <div className="-mt-16">
+        <button className="bg-blue-600 text-white mt-6 px-6 py-2 hover:bg-blue-800 hover:scale-105" onClick={handleReverificationClick}>
+            useReverification() test
+        </button>
+        {/* verification age auto-updates with re-issue of session cookie */}
+        <p className="mt-4">First Factor Verification Age: {session?.factorVerificationAge[0]} minutes</p>
+    </div>
+
   )
 
 
