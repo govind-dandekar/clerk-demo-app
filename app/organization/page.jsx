127b0c939c7938fea4957f33eed14be5784b3b1f
Govind Dandekar <govind.dandekar@gmail.com>
2025-07-02 14:42:11 -0700

    add organization page with OrganizationSwitcher

--- app/organization/page.jsx
+++ app/organization/page.jsx
@@ -1,7 +1,18 @@
-export default function OrganizationHome(){
-    return(
-        <div>
-            Hello World!
-        </div>
-    )
+import { OrganizationSwitcher } from '@clerk/nextjs'
+
+export default function OrganizationSwitcherPage() {
+  return (
+    <div className="-mt-16 bg-blue-600 py-4 px-8 rounded-2xl">
+        <OrganizationSwitcher 
+            afterSelectOrganizationUrl="/"
+            appearance={{
+              elements: {
+                formButtonPrimary: {
+                  fontSize: 18,
+                },
+              },
+            }}
+        />
+    </div>
+  )
 }
